bfe490691a6f5fc6a3c7a1168fad9b42be4bd595
Thomas Arnaud <thomarna@student.42angouleme.fr>
2024-12-04 20:11:25 +0100

    Je capte rien a valgrind

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: thomarna <thomarna@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 11:27:53 by thomarna          #+#    #+#             */
-/*   Updated: 2024/12/04 18:21:19 by thomarna         ###   ########.fr       */
+/*   Updated: 2024/12/04 20:10:57 by thomarna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,41 @@
 #include <stdio.h>
 #include <sys/wait.h>
 
+void	err0r()
+{
+	perror("Error");
+	exit(EXIT_FAILURE);
+}
+
 char	*get_path(char **ep, char *cmd)
 {
 	char	**paths;
 	char	*tmp;
 	char	*path;
+	int	i;
 
+	i = -1;
 	if (access(cmd, X_OK) == 0)
 		return (cmd);
 	while (ft_strnstr(*ep, "PATH", 4) == 0)
 		ep++;
 	if (!ep)
-		return (NULL);
-	*ep += 5;
-	paths = ft_split(*ep, ':');
+		return (0);
+	paths = ft_split(*ep + 5, ':');
 	while (*paths)
 	{
 		tmp = ft_strjoin(*paths, "/");
 		path = ft_strjoin(tmp, cmd);
 		free(tmp);
 		if (access(path, X_OK) == 0)
-			break;
+			return (path);
+		free(path);
 		paths++;
 	}
-	return (path);
+	while (paths[++i])
+		free(paths[i]);
+	free(paths);
+	return (0);
 }
 
 void	execmd(char **ep, char *av)
@@ -48,8 +59,18 @@ void	execmd(char **ep, char *av)
 
 	cmds = ft_split(av, ' ');
 	path = get_path(ep, cmds[0]);
-	ft_dprintf(2, "%s\n", path);
+	if (path == NULL)
+	{
+		while (*cmds)
+		{
+			free(*cmds);
+			cmds++;
+		}
+		free(cmds);
+		err0r();
+	}
 	execve(path, cmds, ep);
+	free(path);
 }
 
 void	parent(int *fd, char **av, char **ep)
@@ -57,6 +78,8 @@ void	parent(int *fd, char **av, char **ep)
 	int	fdout;
 
 	fdout = open(av[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
+	if (fdout < 0)
+		err0r();
 	dup2(fd[0], 0);
 	dup2(fdout, 1);
 	close(fd[1]);
@@ -68,6 +91,8 @@ void	child(int *fd, char **av, char **ep)
 	int	fdin;
 
 	fdin = open(av[1], O_RDONLY);
+	if (fdin < 0)
+		err0r();
 	dup2(fd[1], 1);
 	dup2(fdin, 0);
 	close(fd[0]);
@@ -87,4 +112,5 @@ int	main(int ac, char **av, char **ep)
 		child(fd, av, ep);
 	waitpid(pid, NULL, 0);
 	parent(fd, av, ep);
+	return (0);
 }
