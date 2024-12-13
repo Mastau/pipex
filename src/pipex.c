99126f752e1652ce9bb5315a940e4df83d85def3
Thomas Arnaud <thomarna@student.42angouleme.fr>
2024-12-13 16:32:03 +0100

    Graou

--- src/pipex.c
+++ src/pipex.c
@@ -6,34 +6,13 @@
 /*   By: thomarna <thomarna@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 11:27:53 by thomarna          #+#    #+#             */
-/*   Updated: 2024/12/10 11:24:02 by thomarna         ###   ########.fr       */
+/*   Updated: 2024/12/13 15:18:35 by thomarna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	parent(int *fd, char **av, char **ep, pid_t pid)
-{
-	int	fdout;
-
-	close(fd[1]);
-	fdout = open(av[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
-	if (fdout < 0)
-	{
-		close(fd[0]);
-		err0r(av[4]);
-	}
-	dup2(fd[0], 0);
-	dup2(fdout, 1);
-	close(fd[0]);
-	close(fdout);
-	if (execmd(ep, av[3]))
-		return (err0r(av[3]));
-	waitpid(pid, NULL, 0);
-	return (1);
-}
-
-int	child(int *fd, char **av, char **ep)
+int	child1(int *fd, char **av, char **ep)
 {
 	int	fdin;
 
@@ -42,38 +21,83 @@ int	child(int *fd, char **av, char **ep)
 	if (fdin < 0)
 	{
 		close(fd[1]);
-		err0r(av[1]);
+		return (err0r(av[1]));
 	}
-	dup2(fd[1], 1);
 	dup2(fdin, 0);
-	close(fd[1]);
 	close(fdin);
+	dup2(fd[1], 1);
+	close(fd[1]);
 	if (execmd(ep, av[2]))
 		return (err0r(av[2]));
-	return (0);
+	return (1);
+}
+
+int	child2(int *fd, char **av, char **ep)
+{
+	int	fdout;
+
+	close(fd[1]);
+	fdout = open(av[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
+	if (fdout < 0)
+	{
+		close(fd[0]);
+		return (err0r(av[4]));
+	}
+	dup2(fd[0], 0);
+	close(fd[0]);
+	dup2(fdout, 1);
+	close(fdout);
+	execmd(ep, av[3]);
+	err0r(av[3]);
+	return (1);
 }
 
 int	main(int ac, char **av, char **ep)
 {
 	int		fd[2];
-	pid_t	pid;
-	int		ret;
+	pid_t	pid1;
+	pid_t	pid2;
 
-	ret = 0;
 	if (ac != 5)
 	{
 		ft_dprintf(2, "Usage: %s infile cmd1 cmd2 outfile\n", av[0]);
-		return (EXIT_FAILURE);
+		return (1);
 	}
 	if (pipe(fd) < 0)
 		err0r("");
-	pid = fork();
-	if (pid < 0)
-		err0r("");
-	if (pid == 0)
-		ret = child(fd, av, ep);
-	else
-		ret = parent(fd, av, ep, pid);
-	waitpid(pid, NULL, 0);
-	return (ret);
+	pid1 = fork();
+	if (pid1 == 0)
+		return (child1(fd, av, ep));
+	pid2 = fork();
+	if (pid2 == 0)
+		return (child2(fd, av, ep));
+	close(fd[0]);
+	close(fd[1]);
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
+	return (0);
 }
+
+/* int pipex(char **cmd, char **env) { */
+/* 	int pid1; */
+/* 	int pid2; */
+/*  */
+/* 	pid1 = fork(); */
+/* 	if (!pid1) { */
+/*  */
+/* 	} */
+/* 	else { */
+/* 		pid2 = fork(); */
+/* 		if (!pid2) { */
+/*  */
+/* 		} */
+/* 		else { */
+/*  */
+/* 		} */
+/* 		waitpid(-1, NULL, WUNTRACED); */
+/* 				*/
+/*  */
+/*  */
+/* 	} */
+/*  */
+/* } */
